4e511697fa363e8c67f3111be816745f882d28c2
Atru8729 <a.truskauskas1@gmail.com>
2021-08-04 19:54:10 +0300

    start

---
+++ index.js
@@ -0,0 +1,26 @@
+const _data = require('./lib/data');
+const helpers = require('./lib/helpers');
+const Figura = require('./js/Figura');
+
+(async () => {
+
+    const a = Figura(5, 4);
+
+    const figuruDydziai = Figuros.randomFiguros(5);
+    /*
+    [
+        {width: 5, height: 4},
+        {width: 2, height: 3},
+        {width: 2, height: 7},
+        {width: 1, height: 1},
+        {width: 8, height: 6},
+    ]
+    */
+
+    // sukuriam failus
+
+    // perskaitom failus ir atnaujiname
+
+    // spausdiname visus failu pavadinimus
+
+})();
---
+++ js/Figura.js
@@ -0,0 +1,16 @@
+class Figura {
+    static randomFiguros(amount) {
+        const f = [];
+
+        for () {
+            f.push({})
+        }
+        return f;
+    }
+
+    static size(width, height) {
+
+    }
+}
+
+module.exports = Figura;
